32e056f73f864b9c6f4f9f2673e59a881096c93d
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-02-27 00:47:47 +0600

    Add files via upload

---
+++ JavaScript Output/script.js
@@ -0,0 +1,12 @@
+// Using innerHTML
+document.getElementById('demo1').innerHTML = "This texts showing output using innerHTML";
+
+// Using document.write() 
+// Usually used for testing purposes
+document.write('This paragraph is created by document.write()! The document.write() method should only be used for testing.');
+
+// Using window.alert()
+window.alert('Hi, from window.alert()');
+
+// Using console.log()
+console.log('Hi, from the console.');
