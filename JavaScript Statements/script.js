12b1e393beb88f19745ce9495972c39c5721c328
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-03-01 22:19:36 +0600

    Add files via upload

---
+++ JavaScript Statements/script.js
@@ -0,0 +1,15 @@
+// JavaScript Statements
+
+// Demo 1
+let a, b, c;    // Statement 1
+a = 10;         // Statement 2
+b = 5;          // Statement 3
+c = a + b;      // Statement 4
+
+document.getElementById('demo').innerHTML = 'The value of c is ' + c ;  // Statement 5
+
+// Demo 2
+let x = 9; y = 3; z = 0; // Multiple statements (3) in one line separated by semicolon
+z = x - y;               // Single statement
+
+document.getElementById('demo2').innerHTML = 'The value of z is ' + z ;
