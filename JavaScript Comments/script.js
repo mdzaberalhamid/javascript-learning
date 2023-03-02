39112471ed48bdac6b3592276140596f138f7adc
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-03-02 21:19:46 +0600

    Add files via upload

---
+++ JavaScript Comments/script.js
@@ -0,0 +1,21 @@
+// JavaScript Comments
+
+// Single Line Comment
+
+// Change Paragraph
+document.getElementById('demo1').innerHTML = 'My Paragraph';
+
+// Change Values
+let x = 5;  // Declare x, assigned value 5 to it.
+let y = x + 15; // Declare y, assigned value x + 15 to it.
+
+/* Muli-line
+Comments */
+/*
+This is some text to
+show you how
+multi-line comments 
+works! */
+
+// Comment used for preventing execution of a line/multi-line of codes
+// window.alert('Hey, whats up?');  // Now this line is a comment
