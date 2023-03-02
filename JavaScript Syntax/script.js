17b41ec70b1cfbdf89909b04b354191983229653
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-03-02 20:48:52 +0600

    Add files via upload

---
+++ JavaScript Syntax/script.js
@@ -0,0 +1,30 @@
+// JavaScript Syntax
+
+// Create Variables
+var a;
+let b;
+
+// Use Variables
+a = 10;
+b = 5;
+let c = a + b;
+
+// JavaScript Numbers
+document.getElementById('demo').innerHTML = 10.3;
+
+// JavaScript Strings
+document.getElementById('demo2').innerHTML = 'This is a String';
+
+// JavaScript Variables
+document.getElementById('demo3').innerHTML = c;
+
+// JavaScript Operators
+document.getElementById('demo4').innerHTML = (5 + 6) * 10;
+
+// Case Sensitivity
+let username = 'zaber';
+let UserName = 'Zaber';
+let USERNAME = 'ZABER';
+
+// Try changing different variables
+document.getElementById('demo5').innerHTML = USERNAME;
