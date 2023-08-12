6659f4a336a456de42bedf88992f6e849d022192
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-08-12 20:36:58 +0600

    Add files via upload

---
+++ 11. JavaScript Operators/script.js
@@ -0,0 +1,44 @@
+// document.getElementById("demo").innerHTML = "Test";
+
+// JavaScript Assignment
+let a = 10;
+let b = 5;
+let sum = a + b;
+
+// document.getElementById("demo").innerHTML = "The sum of a and b is: " + sum;
+
+// An example of multiplication
+let multi = a * b;
+
+// document.getElementById("demo").innerHTML = "The multiple of a and b is: " + multi;
+
+// JavaScript Arithmetic Operators
+let d = 3;
+let result1 = (100 - 50) * d;
+// document.getElementById("demo").innerHTML = result1;
+
+// JavaScript Assignment Operators
+let e = 9;
+e += 1;
+// document.getElementById("demo").innerHTML = e;
+
+// JavaScript String Comparison
+let txt1 = "A";
+let txt2 = "B";
+// document.getElementById("demo").innerHTML = txt1 < txt2;
+
+// JavaScript String Addition
+// document.getElementById("demo").innerHTML = txt1 + " " + txt2;
+
+// Example 2
+let txt3 = "What a ";
+txt3 += "nice day!";
+
+// document.getElementById("demo").innerHTML = txt3;
+
+// Adding Strings and Numbers
+let x = 5 + 5;
+let y = "9" + 1;
+let z = "Hi" + 3;
+
+// document.getElementById("demo").innerHTML = x + "<br>" + y + "<br>" + z;
