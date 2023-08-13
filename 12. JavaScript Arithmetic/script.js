f3a930eecdcb09b04a1c32134c7121836c58b015
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-08-13 21:28:29 +0600

    Add files via upload

---
+++ 12. JavaScript Arithmetic/script.js
@@ -0,0 +1,62 @@
+// Arithmetic Operations
+
+// Example 1
+let ex1 = 100 + 20;
+// document.getElementById("demo").innerHTML = ex1;
+
+// Example 2
+let a = 9;
+let b = 6;
+let ex2 = a + b;
+// document.getElementById("demo").innerHTML = ex2;
+
+// Example 3
+let c = 3;
+// document.getElementById("demo").innerHTML = (100 + 10) * c;
+
+// Adding
+let d1 = 5;
+let d2 = 3;
+
+let d3 = d1 + d2;
+// document.getElementById("demo").innerHTML = d3;
+
+// Subtracting
+let d4 = d1 - d2;
+// document.getElementById("demo").innerHTML = d4;
+
+// Multiplying
+let d5 = d1 * d2
+// document.getElementById("demo").innerHTML = d5;
+
+// Dividing
+let d6 = d1 / d2;
+// document.getElementById("demo").innerHTML = d6;
+
+// Remainder
+let d7 = d1 % d2;
+// document.getElementById("demo").innerHTML = "Remainder is: " + d7;
+
+// Incrementing
+let d8 = 9;
+d8++;
+// document.getElementById("demo").innerHTML = "Value after incrementing: " + d8;
+
+// Decrementing
+let d9 = 4;
+d9--;
+// document.getElementById("demo").innerHTML = "Value after decrementing: " + d9;
+
+// Exponentiation
+let e1 = 5;
+let e2 = e1 ** 2;
+// document.getElementById("demo").innerHTML = "Value is: " + e2;
+
+// Operator Precedence
+// Example 1
+let f1 = 100 + 50 * 3;
+// document.getElementById("demo").innerHTML = "Value is: " + f1;
+
+// Example 1
+let f2 = (100 + 50) * 3;
+// document.getElementById("demo").innerHTML = "Value is: " + f2;
