784469a56ecc056882603ce68e046bc5e3fec502
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-08-15 13:53:06 +0600

    Add files via upload

---
+++ 13. Javascript Assignment/script.js
@@ -0,0 +1,86 @@
+// = Operator
+let a = 10;
+// document.getElementById("demo").innerHTML = "Value is: " + a;
+
+let b = 40;
+a = a + b;
+// document.getElementById("demo").innerHTML = "Value is: " + a;
+
+// += Operator
+let c = 8;
+c += 1;
+// document.getElementById("demo").innerHTML = "Value is: " + c;
+
+let txt1 = "Hi, ";
+txt1 += "Welcome!";
+// document.getElementById("demo").innerHTML = txt1;
+
+// -= Operator
+let d = 4;
+d -= 1;
+// document.getElementById("demo").innerHTML = "Value is: " + d;
+
+// *= Operator
+let e = 5;
+e *= 3;
+// document.getElementById("demo").innerHTML = "Value is: " + e;
+
+// **= Operator
+let f = 2;
+f **= 5;
+// document.getElementById("demo").innerHTML = "Value is: " + f;
+
+// /= Operator
+let g = 10;
+g /= 2;
+// document.getElementById("demo").innerHTML = "Value is: " + g;
+
+// %= Operator
+let h = 10;
+h %= 4;
+// document.getElementById("demo").innerHTML = "Value is: " + h;
+
+// <<= Operator
+let i = 1;
+i <<= 2;
+// document.getElementById("demo").innerHTML = "Value is: " + i;
+
+// >>= Operator
+let j = 1;
+j >>= 2;
+// document.getElementById("demo").innerHTML = "Value is: " + j;
+
+// >>>= Operator
+let k = 1;
+k >>>= 2;
+// document.getElementById("demo").innerHTML = "Value is: " + k;
+
+// &= Operator
+let l = 100;
+l &= 5;
+// document.getElementById("demo").innerHTML = "Value is: " + l;
+
+// |= Operator
+let m = 100;
+m |= 5;
+// document.getElementById("demo").innerHTML = "Value is: " + m;
+
+// ^= Operator
+let n = 100;
+n ^= 5;
+// document.getElementById("demo").innerHTML = "Value is: " + n;
+
+// &&= Operator
+let o = 100;
+o &&= 5;
+// document.getElementById("demo").innerHTML = "Value is: " + o;
+
+// ||= Operator
+let p = undefined;
+p ||= 5;
+// document.getElementById("demo").innerHTML = "Value is: " + p;
+
+// ??= Operator
+let q;
+q ??= 3;
+// document.getElementById("demo").innerHTML = "Value is: " + q;
