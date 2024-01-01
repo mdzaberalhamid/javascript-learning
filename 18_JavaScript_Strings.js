f983067e63c3fee4f227d31b8bb676b854df9ac6
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-01-01 23:04:18 +0600

    Create 18_JavaScript_Strings.js

---
+++ 18_JavaScript_Strings.js
@@ -0,0 +1,16 @@
+// JavaScript Strings
+let txt = 'Zaber';
+console.log(txt);
+
+let car1 = 'Marcedes';
+let car2 = "BMW";
+console.log(car1, car2);
+
+let q1 = "It's a ball.";
+console.log(q1);
+
+let q2 = "Your name 'Angus'?";
+console.log(q2);
+
+let q3 = 'Did you see that "cigar" type thing?';
+console.log(q3);
