6ce143ed0fa9ca4a1bd26294b419325bbea25b92
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-01-03 21:19:00 +0600

    Create 19_JavaScript_String_Methods.js

---
+++ 19_JavaScript_String_Methods.js
@@ -0,0 +1,16 @@
+// JavaScript String Methods
+
+// JavaScript String Length
+txt1 = 'This is a string';
+console.log(txt1.length);       // 16
+
+// Extracting String Characters
+txt2 = 'Hello world!';
+console.log(txt2.charAt(0));    // H
+console.log(txt2.charCodeAt(0));    // 72
+
+txt3 = 'W3Schools';
+console.log(txt3.at(1));        // 3
+console.log(txt3[0]);           // W
+
+// Extracting String Parts
