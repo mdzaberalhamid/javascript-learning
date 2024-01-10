4b487a668a57ad48c9249210ef443cd93b5caf54
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-01-10 22:01:44 +0600

    Update 19_JavaScript_String_Methods.js

--- 19_JavaScript_String_Methods.js
+++ 19_JavaScript_String_Methods.js
@@ -38,3 +38,48 @@ console.log(txt7.concat(" ", "World!"));    // Hello World!
 console.log(txt7);              // Hello
 
 // JavaScript String trim()
+let txt7 = "     Javascript";
+console.log(txt7.length);   // 15
+let txt8 = txt7.trim();
+console.log(txt8.length);   // 10
+
+let txt9 = "   Hi ";
+console.log(txt9.length);   // 6
+let txt10 = txt9.trimStart();
+console.log(txt10.length);  // 3
+let txt11 = txt9.trimEnd();
+console.log(txt11.length);  // 5
+
+// JavaScript String Padding
+let txt12 = "5";
+let txt13 = txt12.padStart(4,"1");
+console.log(txt13);         // 1115
+let txt14 = txt12.padEnd(3,"H");
+console.log(txt14);         // 5HH
+let num1 = 3;
+let str1 = num1.toString();
+console.log(typeof str1);   // string
+console.log(str1.padEnd(4,"9")); // 3999
+
+// JavaScript String repeat()
+let txt15 = "JS";
+console.log(txt15.repeat(2));   // JSJS
+
+// Replacing String Content
+let txt16 = "I\'m learning Javascript!";
+console.log(txt16);
+console.log(txt16.replace("Javascript","React"));
+
+// JavaScript String ReplaceAll()
+let text = "I like dogs. Dogs are very easy to pet. Dogs are very popular.";
+console.log(text);
+text = text.replaceAll("dogs","cats");
+text = text.replaceAll("Dogs","Cats");
+console.log(text);
+
+// JavaScript String split()
+let str2 = "a,b,c,d,e";
+const arr = str2.split(",");
+console.log(arr[0]);    // a
+console.log(arr[3]);    // d
+
