edf78876d713679304a0972069eb9ce9ffae7d2d
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-01-28 22:20:07 +0600

    Create 20_JavaScript_String_Search.js

---
+++ 20_JavaScript_String_Search.js
@@ -0,0 +1,23 @@
+// JavaScript String Search
+console.log("JavaScript String Search");
+
+// JavaScript String indexOf()
+let txt1 = "This is a text with some additional text";
+let o1 = txt1.indexOf("text");
+console.log(o1);    // 10
+
+// JavaScript String lastIndexOf()
+let txt2 = "This is a text with some additional text";
+let o2 = txt2.lastIndexOf("text");
+console.log(o2);    // 36
+
+//
+let o3 = txt1.indexOf("text", 14);
+console.log(o3);    // 36
+
+//
+let txt3 = "Please locate where 'locate' occurs!";
+let o4 = txt3.lastIndexOf("locate", 15);
+console.log(o4);    // 7
+
+// JavaScript String search()
